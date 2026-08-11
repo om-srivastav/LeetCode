6c8f1a5776ef9c317977a9e80c208e65dadd753a
Om Srivastav <omsrivastav7030@gmail.com>
2026-08-11 12:31:20 +0530

    Time: 0 ms (100%), Space: 8.8 MB (89.77%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,28 @@
+#include <stack>
+using namespace std;
+
+class Solution {
+public:
+    bool isValid(string s) {
+            stack<char> st;
+
+                    for(char c : s) {
+
+                                if(c == '(' || c == '{' || c == '[') {
+                                                st.push(c);
+                                                            }
+                                                                        else {
+
+                                                                                        if(st.empty()) return false;
+
+                                                                                                        if(c == ')' && st.top() != '(') return false;
+                                                                                                                        if(c == '}' && st.top() != '{') return false;
+                                                                                                                                        if(c == ']' && st.top() != '[') return false;
+
+                                                                                                                                                        st.pop();
+                                                                                                                                                                    }
+                                                                                                                                                                            }
+
+                                                                                                                                                                                    return st.empty();
+                                                                                                                                                                                        }
+                                                                                                                                                                                        };
