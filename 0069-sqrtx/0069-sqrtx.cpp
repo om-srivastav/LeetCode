45cba57deac63bb57a378ce60f237b7790a4f4e7
Om Srivastav <omsrivastav7030@gmail.com>
2026-09-26 10:15:29 +0530

    Time: 59 ms (5.07%), Space: 8.5 MB (50.76%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        return sqrt(x);
+        for(int i=1;i<=x;i++){
+            if(i==x/i)return i;
+            if(i>x/i)return i-1;
+        }
+        return 0;
     }
 };
